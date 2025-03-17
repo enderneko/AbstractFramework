a9c0ae8593665e9846afcff0ffcbe3279b88b88a
enderneko <enderneko-dev@outlook.com>
2025-03-17 15:01:50 +0800

    Update .test

---
+++ .test/Atlas.lua
@@ -0,0 +1,31 @@
+---@class AbstractFramework
+local AF = _G.AbstractFramework
+
+local atlasBase = {
+    Anchors = {256, 256},
+}
+
+local atlasInfo = {
+    -- Anchors
+    Anchor_BOTTOM = {"Anchors", 0, 0, 64, 64},
+    Anchor_BOTTOMLEFT = {"Anchors", 64, 0, 64, 64},
+    Anchor_BOTTOMRIGHT = {"Anchors", 128, 0, 64, 64},
+    Anchor_CENTER = {"Anchors", 192, 0, 64, 64},
+    Anchor_LEFT = {"Anchors", 0, 64, 64, 64},
+    Anchor_RIGHT = {"Anchors", 64, 64, 64, 64},
+    Anchor_TOP = {"Anchors", 128, 64, 64, 64},
+    Anchor_TOPLEFT = {"Anchors", 192, 64, 64, 64},
+    Anchor_TOPRIGHT = {"Anchors", 0, 128, 64, 64},
+}
+
+---@param texture Texture
+---@param name string
+function AF.SetAtlas(texture, name)
+    local atlas = atlases[name]
+    if atlas then
+        local base, x, y, w, h = AF.Unpack5(atlas)
+        local bW, bH = AF.Unpack2(atlasBase[base])
+        texture:SetTexture("Interface\\AddOns\\AbstractFramework\\Media\\Atlases\\" .. base)
+        texture:SetTexCoord(x / bW, (x + w) / bW, y / bH, (y + h) / bH)
+    end
+end
