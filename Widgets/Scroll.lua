0f8d4beeda523c595e72b04ac28bdf53a9f24d08
enderneko <enderneko-dev@outlook.com>
2025-04-02 20:52:30 +0800

    Implement AF.CreateScrollGrid

--- Widgets/Scroll.lua
+++ Widgets/Scroll.lua
@@ -1,8 +1,9 @@
 ---@class AbstractFramework
 local AF = _G.AbstractFramework
 
-local abs, max, select = abs, max, select
+local select, abs, max, ceil = select, abs, max, ceil
 local Round = AF.Round
+local GetCursorPosition = GetCursorPosition
 
 ---------------------------------------------------------------------
 -- scroll frame
@@ -223,7 +224,7 @@ function AF_ScrollListMixin:UpdateSlots()
             if i == 1 then
                 AF.SetPoint(self.slots[i], "TOPLEFT", self.horizontalMargin, 0)
             else
-                AF.SetPoint(self.slots[i], "TOPLEFT", self.slots[i-1], "BOTTOMLEFT", 0, -self.slotSpacing)
+                AF.SetPoint(self.slots[i], "TOPLEFT", self.slots[i - 1], "BOTTOMLEFT", 0, -self.slotSpacing)
             end
         end
 
@@ -237,7 +238,7 @@ function AF_ScrollListMixin:UpdateSlots()
         end
     end
     -- hide unused slots
-    for i = self.slotNum+1, #self.slots do
+    for i = self.slotNum + 1, #self.slots do
         self.slots[i]:Hide()
         if self.slots[i].widget then
             self.slots[i].widget:Hide()
@@ -362,11 +363,17 @@ function AF_ScrollListMixin:ScrollToBottom()
     self:SetScroll(self.widgetNum - self.slotNum + 1)
 end
 
----@return number index the first shown widget index
----@return Frame widget the first shown widget
 function AF_ScrollListMixin:GetScroll()
-    if self.widgetNum == 0 then return end
-    return self.slots[1].widgetIndex, self.slots[1].widget
+    if not self:CanScroll() then
+        return 1
+    end
+    return self.slots[1].widgetIndex
+end
+
+function AF_ScrollListMixin:GetWidgetAt(index)
+    if index and index > 0 and index <= self.slotNum then
+        return self.slots[index].widget
+    end
 end
 
 function AF_ScrollListMixin:GetScrollRange()
@@ -517,4 +524,328 @@ function AF.CreateScrollList(parent, name, verticalMargin, horizontalMargin, slo
     AF.AddToPixelUpdater(scrollList)
 
     return scrollList
+end
+
+
+---------------------------------------------------------------------
+-- scrol grid
+---------------------------------------------------------------------
+---@class AF_ScrollGrid:AF_BorderedFrame
+local AF_ScrollGridMixin = {}
+
+---@private
+function AF_ScrollGridMixin:UpdateSlots()
+    for i = 1, self.slotNum do
+        if not self.slots[i] then
+            self.slots[i] = AF.CreateFrame(self.slotFrame)
+            AF.RemoveFromPixelUpdater(self.slots[i])
+            AF.SetSize(self.slots[i], self.slotWidth, self.slotHeight)
+            if i == 1 then
+                AF.SetPoint(self.slots[i], "TOPLEFT")
+            elseif i % self.slotColumn == 1 then
+                AF.SetPoint(self.slots[i], "TOPLEFT", self.slots[i - self.slotColumn], "BOTTOMLEFT", 0, -self.slotSpacing)
+            else
+                AF.SetPoint(self.slots[i], "TOPLEFT", self.slots[i - 1], "TOPRIGHT", self.slotSpacing, 0)
+            end
+        end
+
+        self.slots[i]:Show()
+
+        if self.slots[i].widget then
+            self.slots[i].widget:Show()
+            if self.slots[i].widget.UpdatePixels then
+                self.slots[i].widget:UpdatePixels()
+            end
+        end
+    end
+
+    -- hide unused slots
+    for i = self.slotNum + 1, #self.slots do
+        self.slots[i]:Hide()
+        if self.slots[i].widget then
+            self.slots[i].widget:Hide()
+        end
+    end
+end
+
+function AF_ScrollGridMixin:SetSlotRowsAndColumns(newSlotRow, newSlotColumn)
+    self.slotRow = newSlotRow
+    self.slotColumn = newSlotColumn
+    self.slotNum = newSlotRow * newSlotColumn
+    AF.SetGridSize(self, self.slotWidth, self.slotHeight, self.slotSpacing, self.slotSpacing, newSlotColumn, newSlotRow, self.verticalMargin, self.verticalMargin, self.horizontalMargin, self.horizontalMargin)
+    self:UpdateSlots()
+end
+
+function AF_ScrollGridMixin:SetSlotSize(newWidth, newHeight)
+    self.slotWidth = newWidth
+    self.slotHeight = newHeight
+    self:SetSlotRowsAndColumns(self.slotRow, self.slotColumn)
+end
+
+function AF_ScrollGridMixin:SetWidgets(widgets)
+    self:Reset()
+    self.widgets = widgets
+    self.widgetNum = #widgets
+    self:SetScroll(1)
+
+    -- call UpdatePixels on show
+    for _, w in ipairs(self.widgets) do
+        AF.RemoveFromPixelUpdater(w)
+    end
+
+    if self.widgetNum > self.slotNum then -- can scroll
+        self.scrollBar:Show()
+        local p = self.slotRow / ceil(self.widgetNum / self.slotColumn)
+        self.scrollThumb:SetHeight(self.scrollBar:GetHeight() * p)
+        AF.SetPoint(self.slotFrame, "BOTTOMRIGHT", -7-self.horizontalMargin, self.verticalMargin)
+    else
+        self.scrollBar:Hide()
+        AF.SetPoint(self.slotFrame, "BOTTOMRIGHT", -self.horizontalMargin, self.verticalMargin)
+    end
+end
+
+-- reset
+function AF_ScrollGridMixin:Reset()
+    self.widgets = {}
+    self.widgetNum = 0
+    -- hide slot widgets
+    for _, s in ipairs(self.slots) do
+        if s.widget then
+            s.widget:Hide()
+        end
+        s.widget = nil
+        s.widgetIndex = nil
+    end
+    -- resize / repoint
+    AF.SetPoint(self.slotFrame, "BOTTOMRIGHT", -self.horizontalMargin, self.verticalMargin)
+    self.scrollBar:Hide()
+end
+
+---@param startRow number
+function AF_ScrollGridMixin:SetScroll(startRow)
+    if not startRow then return end
+    if startRow <= 0 or startRow > self:GetScrollRange() + 1 then return end
+
+    local total = self.widgetNum
+    local from = (startRow - 1) * self.slotColumn + 1
+    local to = from + self.slotNum - 1
+
+    -- not enough widgets (fill from the first)
+    if total <= self.slotNum then
+        from = 1
+        to = total
+
+    -- have enough widgets, but result in empty row, fix it
+    elseif ceil(total / self.slotColumn) - startRow + 1 < self.slotRow then
+        from = (floor(total / self.slotColumn) - 1) * self.slotColumn + 1
+        to = total
+    end
+
+    if self.slots[1].widgetIndex == from then
+        return
+    end
+
+    -- fill
+    local slotIndex = 1
+    for i, w in ipairs(self.widgets) do
+        w:ClearAllPoints()
+        if i < from or i > to then
+            w:Hide()
+        else
+            w:Show()
+            w:SetAllPoints(self.slots[slotIndex])
+            if w.UpdatePixels then
+                w:UpdatePixels()
+            end
+            w._slotIndex = slotIndex
+            if w.Update then
+                -- NOTE: fix some widget issues, define them manually
+                w:Update()
+            end
+            self.slots[slotIndex].widget = w
+            self.slots[slotIndex].widgetIndex = i
+            slotIndex = slotIndex + 1
+        end
+    end
+
+    -- reset empty slots
+    for i = slotIndex, self.slotNum do
+        self.slots[i].widget = nil
+        self.slots[slotIndex].widgetIndex = nil
+    end
+
+    -- update scorll thumb
+    if self:CanScroll() then
+        local offset = (startRow - 1) * ((self.scrollBar:GetHeight() - self.scrollThumb:GetHeight()) / self:GetScrollRange()) -- n * perHeight
+        self.scrollThumb:SetPoint("TOP", 0, -offset)
+    end
+end
+
+function AF_ScrollGridMixin:ScrollToBottom()
+    self:SetScroll(ceil(self.widgetNum / self.slotColumn) - self.slotRow + 1)
+end
+
+function AF_ScrollGridMixin:GetScroll()
+    if not self:CanScroll() then
+        return 1
+    end
+    return ceil(self.slots[1].widgetIndex / self.slotColumn)
+end
+
+function AF_ScrollGridMixin:GetWidgetAt(index)
+    if index and index > 0 and index <= self.slotNum then
+        return self.slots[index].widget
+    end
+end
+
+function AF_ScrollGridMixin:GetScrollRange()
+    local range = ceil(self.widgetNum / self.slotColumn) - self.slotRow
+    return range <= 0 and 0 or range
+end
+
+function AF_ScrollGridMixin:CanScroll()
+    return self.widgetNum > self.slotNum
+end
+
+function AF_ScrollGridMixin:SetScrollStep(step)
+    self.step = step
+end
+
+function AF_ScrollGridMixin:UpdatePixels()
+    AF.ReSize(self)
+    AF.RePoint(self)
+    AF.ReBorder(self)
+    AF.RePoint(self.slotFrame)
+    self.scrollBar:UpdatePixels()
+
+    -- update slots and widgets
+    for _, s in ipairs(self.slots) do
+        s:UpdatePixels()
+        if s.widget and s.widget.UpdatePixels then
+            s.widget:UpdatePixels()
+        end
+    end
+
+    -- update scorll thumb
+    if self:CanScroll() then
+        local offset = (self:GetScroll() - 1) * ((self.scrollBar:GetHeight() - self.scrollThumb:GetHeight()) / self:GetScrollRange()) -- n * perHeight
+        self.scrollThumb:SetPoint("TOP", 0, -offset)
+    end
+end
+
+---@param verticalMargin number top/bottom margin
+---@param horizontalMargin number left/right margin
+---@param slotSpacing number spacing between widgets next to each other
+---@return AF_ScrollGrid scrollList
+function AF.CreateScrollGrid(parent, name, verticalMargin, horizontalMargin, slotColumn, slotRow, slotWidth, slotHeight, slotSpacing, color, borderColor)
+    local scrollGrid = AF.CreateBorderedFrame(parent, name, nil, nil, color, borderColor)
+    AF.SetGridSize(scrollGrid, slotWidth, slotHeight, slotSpacing, slotSpacing, slotColumn, slotRow, verticalMargin, verticalMargin, horizontalMargin, horizontalMargin)
+
+    scrollGrid.slotColumn = slotColumn
+    scrollGrid.slotRow = slotRow
+    scrollGrid.slotNum = slotColumn * slotRow
+    scrollGrid.slotWidth = slotWidth
+    scrollGrid.slotHeight = slotHeight
+    scrollGrid.slotSpacing = slotSpacing
+    scrollGrid.verticalMargin = verticalMargin
+    scrollGrid.horizontalMargin = horizontalMargin
+
+    -- slotFrame
+    local slotFrame = CreateFrame("Frame", nil, scrollGrid)
+    scrollGrid.slotFrame = slotFrame
+    AF.SetPoint(slotFrame, "TOPLEFT", horizontalMargin, -verticalMargin)
+    AF.SetPoint(slotFrame, "BOTTOMRIGHT", -horizontalMargin, verticalMargin)
+
+    -- scrollBar
+    local scrollBar = AF.CreateBorderedFrame(scrollGrid, nil, 5, nil, color, borderColor)
+    scrollGrid.scrollBar = scrollBar
+    AF.SetPoint(scrollBar, "TOPRIGHT", 0, -verticalMargin)
+    AF.SetPoint(scrollBar, "BOTTOMRIGHT", 0, verticalMargin)
+    scrollBar:Hide()
+
+    -- scrollBar thumb
+    local scrollThumb = AF.CreateBorderedFrame(scrollBar, nil, 5, nil, AF.GetAccentColorTable(0.7))
+    scrollGrid.scrollThumb = scrollThumb
+    scrollThumb.r, scrollThumb.g, scrollThumb.b = AF.GetAccentColorRGB()
+    -- AF.SetPoint(scrollThumb, "TOP")
+    scrollThumb:EnableMouse(true)
+    scrollThumb:SetMovable(true)
+    scrollThumb:SetHitRectInsets(-5, -5, 0, 0) -- Frame:SetHitRectInsets(left, right, top, bottom)
+    scrollThumb:SetScript("OnEnter", function()
+        scrollThumb:SetBackdropColor(scrollThumb.r, scrollThumb.g, scrollThumb.b, 0.9)
+    end)
+    scrollThumb:SetScript("OnLeave", function()
+        scrollThumb:SetBackdropColor(scrollThumb.r, scrollThumb.g, scrollThumb.b, 0.7)
+    end)
+
+    -- slots
+    scrollGrid.slots = {}
+
+    Mixin(scrollGrid, AF_ScrollGridMixin)
+    scrollGrid:UpdateSlots()
+
+    -- items
+    scrollGrid.widgets = {}
+    scrollGrid.widgetNum = 0
+
+    -- for mouse wheel ----------------------------------------------
+    scrollGrid:SetScrollStep(1)
+
+    -- enable mouse wheel scroll
+    scrollGrid:EnableMouseWheel(true)
+    scrollGrid:SetScript("OnMouseWheel", function(self, delta)
+        if not scrollGrid:CanScroll() then return end
+        if delta == 1 then -- scroll up
+            scrollGrid:SetScroll(scrollGrid:GetScroll() - scrollGrid.step)
+        elseif delta == -1 then -- scroll down
+            scrollGrid:SetScroll(scrollGrid:GetScroll() + scrollGrid.step)
+        end
+    end)
+    -----------------------------------------------------------------
+
+    -- dragging and scrolling ---------------------------------------
+    scrollThumb:SetScript("OnMouseDown", function(self, button)
+        if button ~= "LeftButton" then return end
+
+        local scale = scrollThumb:GetEffectiveScale()
+        local offsetY = select(5, scrollThumb:GetPoint(1))
+        local mouseY = select(2, GetCursorPosition()) / scale -- https://warcraft.wiki.gg/wiki/API_GetCursorPosition
+
+        self:SetScript("OnUpdate", function(self)
+            local newMouseY = select(2, GetCursorPosition()) / scale
+            local mouseOffset = newMouseY - mouseY
+            local newOffsetY = offsetY + mouseOffset
+
+            -- top ------------------------------
+            if newOffsetY >= 0 then
+                if scrollGrid:GetScroll() ~= 1 then
+                    scrollGrid:SetScroll(1)
+                end
+
+            -- bottom ---------------------------
+            elseif (-newOffsetY) + scrollThumb:GetHeight() >= scrollBar:GetHeight() then
+                if scrollGrid:GetScroll() ~= scrollGrid:GetScrollRange() + 1 then
+                    scrollGrid:SetScroll(scrollGrid:GetScrollRange() + 1)
+                end
+
+            -- scroll ---------------------------
+            else
+                local threshold = (scrollBar:GetHeight() - scrollThumb:GetHeight()) / scrollGrid:GetScrollRange()
+                local targetIndex = Round(abs(newOffsetY) / threshold)
+                targetIndex = max(targetIndex, 1)
+                if targetIndex ~= scrollGrid:GetScroll() then
+                    scrollGrid:SetScroll(targetIndex)
+                end
+            end
+        end)
+    end)
+
+    scrollThumb:SetScript("OnMouseUp", function(self)
+        self:SetScript("OnUpdate", nil)
+    end)
+    -----------------------------------------------------------------
+
+    AF.AddToPixelUpdater(scrollGrid)
+
+    return scrollGrid
 end
