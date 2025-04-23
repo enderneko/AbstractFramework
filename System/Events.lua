d93c74ef8f4e7fd98ff9b1584f7c4263c9ba9868
enderneko <enderneko-dev@outlook.com>
2025-04-23 16:38:38 +0800

    Add Events

--- System/Events.lua
+++ System/Events.lua
@@ -7,21 +7,29 @@ AF.AddEventHandler(AF)
 -- instance
 ---------------------------------------------------------------------
 local IsInInstance = IsInInstance
-local isInInstance = false
+local wasInInstance = nil
+
+--* AF_INSTANCE_ENTER / AF_INSTANCE_LEAVE
+-- payload: instanceType, wasInInstance
+-- wasInInstance:
+--     nil if first time after login
+--     true if the player was in an instance before
+--     false if not
 
 AF:RegisterEvent("PLAYER_ENTERING_WORLD", function()
     local isIn, iType = IsInInstance()
     if isIn then
-        isInInstance = true
-        AF.Fire("AF_INSTANCE_ENTER", iType)
-    elseif isInInstance then
-        isInInstance = false
-        AF.Fire("AF_INSTANCE_LEAVE", iType)
+        AF.Fire("AF_INSTANCE_ENTER", iType, wasInInstance)
+        wasInInstance = true
+    else
+        AF.Fire("AF_INSTANCE_LEAVE", iType, wasInInstance)
+        wasInInstance = false
     end
 end)
 
 ---------------------------------------------------------------------
 -- combat
----------------------------------------------------------------------
+--------------------------------------------------------------------
+--* AF_COMBAT_ENTER / AF_COMBAT_LEAVE
 AF:RegisterEvent("PLAYER_REGEN_DISABLED", AF.GetFireFunc("AF_COMBAT_ENTER"))
 AF:RegisterEvent("PLAYER_REGEN_ENABLED", AF.GetFireFunc("AF_COMBAT_LEAVE"))
