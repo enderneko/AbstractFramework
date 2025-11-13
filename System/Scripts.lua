5c8e12ea05e26dd993a86b4a31d4baba5526a361
enderneko <enderneko-dev@outlook.com>
2025-11-13 11:02:31 +0800

    Add AF.CompileWithEnv

--- System/Scripts.lua
+++ System/Scripts.lua
@@ -10,17 +10,30 @@ function AF.CompileWithGlobalEnv(script, env)
         return nil, "empty script"
     end
 
-    local newEnv = setmetatable({}, { __index = _G })
+    local newEnv = setmetatable(type(env) == "table" and env or {}, {__index = _G})
 
-    if type(env) == "table" then
-        AF.MergeRaw(newEnv, env)
+    local chunk, err = loadstring(script)
+    if not chunk then return nil, err end
+
+    setfenv(chunk, newEnv)
+
+    return chunk
+end
+
+---@param script string lua code to compile
+---@param env table environment table to set as the chunk's environment
+---@return function|nil compiled chunk when successful
+---@return string|nil error message when compilation fails
+function AF.CompileWithEnv(script, env)
+    if type(script) ~= "string" or script == "" then
+        return nil, "empty script"
     end
 
     local chunk, err = loadstring(script)
     if not chunk then return nil, err end
 
-    if setfenv then
-        setfenv(chunk, newEnv)
+    if env then
+        setfenv(chunk, env)
     end
 
     return chunk
