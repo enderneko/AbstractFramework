92c17c94a4241682ee2046321c8da2829ff055b2
enderneko <enderneko-dev@outlook.com>
2025-12-02 14:07:07 +0800

    Update Color.lua

--- Widgets/Color.lua
+++ Widgets/Color.lua
@@ -32,6 +32,7 @@ local COLORS = {
 
     -- for regions
     ["background"] = {["hex"] = "d91a1a1a", ["t"] = {0.1, 0.1, 0.1, 0.85}},
+    ["background2"] = {["hex"] = "d9333333", ["t"] = {0.2, 0.2, 0.2, 0.85}},
     ["border"] = {["hex"] = "ff000000", ["t"] = {0, 0, 0, 1}},
     ["header"] = {["hex"] = "ff202020", ["t"] = {0.127, 0.127, 0.127, 1}}, -- header background
     ["widget"] = {["hex"] = "ff262626", ["t"] = {0.15, 0.15, 0.15, 1}}, -- widget background
