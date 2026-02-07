d8df6c507cc13b90542a2e4b75da04f561ec92fb
enderneko <enderneko-dev@outlook.com>
2026-02-07 17:34:34 +0800

    Add AF_BaseStatusBarMixin

--- Widgets/StatusBar.lua
+++ Widgets/StatusBar.lua
@@ -1,6 +1,8 @@
 ---@class AbstractFramework
 local AF = select(2, ...)
+
 local CreateColor = CreateColor
+local UnpackColor = AF.UnpackColor
 
 ---------------------------------------------------------------------
 -- status bar countdown
@@ -118,6 +120,112 @@ function AF.CreateBlizzardStatusBar(parent, minValue, maxValue, width, height, c
 end
 
 ---------------------------------------------------------------------
+-- AF_BaseStatusBar
+---------------------------------------------------------------------
+---@class AF_BaseStatusBar
+AF_BaseStatusBarMixin = {}
+
+---@param fillTexture string
+---@param unfillTexture string|nil if nil then use fillTexture
+---@param wrapModeHorizontal string|nil
+---@param wrapModeVertical string|nil
+---@param filterMode string|nil
+function AF_BaseStatusBarMixin:SetTexture(fillTexture, unfillTexture, wrapModeHorizontal, wrapModeVertical, filterMode)
+    self.fill:SetTexture(fillTexture, wrapModeHorizontal, wrapModeVertical, filterMode)
+    self.unfill:SetTexture(unfillTexture or fillTexture, wrapModeHorizontal, wrapModeVertical, filterMode)
+end
+
+function AF_BaseStatusBarMixin:LSM_SetTexture(fillTexture, unfillTexture, wrapModeHorizontal, wrapModeVertical, filterMode)
+    fillTexture = AF.LSM_GetBarTexture(fillTexture)
+    unfillTexture = unfillTexture and AF.LSM_GetBarTexture(unfillTexture) or fillTexture
+    self:SetTexture(fillTexture, unfillTexture, wrapModeHorizontal, wrapModeVertical, filterMode)
+end
+
+function AF_BaseStatusBarMixin:SetFillColor(r, g, b, a)
+    self.fill:SetVertexColor(r, g, b, a)
+end
+
+---@param orientation Orientation|nil
+---@param ... number|table (r1, g1, b1, a1, r2, g2, b2, a2) or {startColorTable, endColorTable}
+function AF_BaseStatusBarMixin:SetGradientFillColor(orientation, ...)
+    if select("#", ...) == 2 then
+        local startColor, endColor = ...
+        self.fill:SetGradient(orientation or "HORIZONTAL", CreateColor(UnpackColor(startColor)), CreateColor(UnpackColor(endColor)))
+    else
+        local r1, g1, b1, a1, r2, g2, b2, a2 = ...
+        self.fill:SetGradient(orientation or "HORIZONTAL", CreateColor(r1, g1, b1, a1), CreateColor(r2, g2, b2, a2))
+    end
+end
+
+function AF_BaseStatusBarMixin:SetUnfillColor(r, g, b, a)
+    self.unfill:SetVertexColor(r, g, b, a)
+end
+
+----@param orientation Orientation|nil
+---@param ... number|table (r1, g1, b1, a1, r2, g2, b2, a2) or {startColorTable, endColorTable}
+function AF_BaseStatusBarMixin:SetGradientUnfillColor(orientation, ...)
+    if select("#", ...) == 2 then
+        local startColor, endColor = ...
+        self.unfill:SetGradient(orientation or "HORIZONTAL", CreateColor(UnpackColor(startColor)), CreateColor(UnpackColor(endColor)))
+    else
+        local r1, g1, b1, a1, r2, g2, b2, a2 = ...
+        self.unfill:SetGradient(orientation or "HORIZONTAL", CreateColor(r1, g1, b1, a1), CreateColor(r2, g2, b2, a2))
+    end
+end
+
+-- function AF_BaseStatusBarMixin:SetGradient(gradientType)
+--     if gradientType == "disabled" then
+--         self.fill.gradientMask:Hide()
+--         self.unfill.gradientMask:Hide()
+--     else
+--         self.fill.gradientMask:Show()
+--         self.unfill.gradientMask:Show()
+
+--         if gradientType == "horizontal" then
+--             self.fill.gradientMask:SetTexture(AF.GetTexture("Gradient_Linear_Left"), "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE", "NEAREST")
+--             self.unfill.gradientMask:SetTexture(AF.GetTexture("Gradient_Linear_Left"), "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE", "NEAREST")
+--         end
+--     end
+-- end
+
+function AF_BaseStatusBarMixin:SetBackgroundColor(r, g, b, a)
+    self:SetBackdropColor(r, g, b, a)
+end
+
+function AF_BaseStatusBarMixin:SetBorderColor(r, g, b, a)
+    self:SetBackdropBorderColor(r, g, b, a)
+end
+
+function AF_BaseStatusBarMixin:EnableBorder(enabled)
+    if enabled then
+        AF.ApplyDefaultBackdrop(self)
+        AF.SetOnePixelInside(self.innerBar)
+    else
+        AF.ApplyDefaultBackdrop_NoBorder(self)
+        AF.SetAllPoints(self.innerBar)
+    end
+end
+
+function AF_BaseStatusBarMixin:GetBarSize()
+    return self.innerBar:GetSize()
+end
+
+function AF_BaseStatusBarMixin:GetBarWidth()
+    return self.innerBar:GetWidth()
+end
+
+function AF_BaseStatusBarMixin:GetBarHeight()
+    return self.innerBar:GetHeight()
+end
+
+function AF_BaseStatusBarMixin:DefaultUpdatePixels()
+    AF.ReSize(self)
+    AF.RePoint(self)
+    AF.ReBorder(self)
+    AF.RePoint(self.innerBar)
+end
+
+---------------------------------------------------------------------
 -- simple
 ---------------------------------------------------------------------
 local ClampedPercentageBetween = AF.ClampedPercentageBetween
@@ -153,102 +261,38 @@ local function UpdateValue_Vertical(self)
     end
 end
 
----@class AF_SimpleStatusBar:AF_SmoothStatusBar,Frame
+---@class AF_SimpleStatusBar:AF_BaseStatusBar,AF_SmoothStatusBar,Frame
 local AF_SimpleStatusBarMixin = {}
-
----@param texture string
----@param lossTexture string|nil if nil then use texture
----@param wrapModeHorizontal string|nil
----@param wrapModeVertical string|nil
----@param filterMode string|nil
-function AF_SimpleStatusBarMixin:SetTexture(texture, lossTexture, wrapModeHorizontal, wrapModeVertical, filterMode)
-    self.fill:SetTexture(texture, wrapModeHorizontal, wrapModeVertical, filterMode)
-    self.loss:SetTexture(lossTexture or texture, wrapModeHorizontal, wrapModeVertical, filterMode)
-end
-
-function AF_SimpleStatusBarMixin:SetColor(r, g, b, a)
-    self.fill:SetVertexColor(r, g, b, a)
-end
-
----@param orientation Orientation|nil
----@param ... number|table (r1, g1, b1, a1, r2, g2, b2, a2) or {startColorTable, endColorTable}
-function AF_SimpleStatusBarMixin:SetGradientColor(orientation, ...)
-    if select("#", ...) == 2 then
-        local startColor, endColor = ...
-        self.fill:SetGradient(orientation or "HORIZONTAL", CreateColor(AF.UnpackColor(startColor)), CreateColor(AF.UnpackColor(endColor)))
-    else
-        local r1, g1, b1, a1, r2, g2, b2, a2 = ...
-        self.fill:SetGradient(orientation or "HORIZONTAL", CreateColor(r1, g1, b1, a1), CreateColor(r2, g2, b2, a2))
-    end
-end
-
-function AF_SimpleStatusBarMixin:SetLossColor(r, g, b, a)
-    self.loss:SetVertexColor(r, g, b, a)
-end
-
-----@param orientation Orientation|nil
----@param ... number|table (r1, g1, b1, a1, r2, g2, b2, a2) or {startColorTable, endColorTable}
-function AF_SimpleStatusBarMixin:SetGradientLossColor(orientation, ...)
-    if select("#", ...) == 2 then
-        local startColor, endColor = ...
-        self.loss:SetGradient(orientation or "HORIZONTAL", CreateColor(AF.UnpackColor(startColor)), CreateColor(AF.UnpackColor(endColor)))
-    else
-        local r1, g1, b1, a1, r2, g2, b2, a2 = ...
-        self.loss:SetGradient(orientation or "HORIZONTAL", CreateColor(r1, g1, b1, a1), CreateColor(r2, g2, b2, a2))
-    end
-end
-
-function AF_SimpleStatusBarMixin:SetBackgroundColor(r, g, b, a)
-    self:SetBackdropColor(r, g, b, a)
-end
-
-function AF_SimpleStatusBarMixin:SetBorderColor(r, g, b, a)
-    self:SetBackdropBorderColor(r, g, b, a)
-end
-
-function AF_SimpleStatusBarMixin:EnableBorder(enabled)
-    if enabled then
-        AF.ApplyDefaultBackdrop(self)
-        AF.SetOnePixelInside(self.inner)
-        AF.SetOnePixelInside(self.fill)
-        AF.SetOnePixelInside(self.loss)
-    else
-        AF.ApplyDefaultBackdrop_NoBorder(self)
-        AF.SetAllPoints(self.inner)
-        AF.SetAllPoints(self.fill)
-        AF.SetAllPoints(self.loss)
-    end
-end
 
 ---@param orientation "left_to_right"|"right_to_left"|"bottom_to_top"|"top_to_bottom"
 function AF_SimpleStatusBarMixin:SetOrientation(orientation)
     self.fill.mask:ClearAllPoints()
-    self.loss.mask:ClearAllPoints()
+    self.unfill.mask:ClearAllPoints()
 
     if orientation == "left_to_right" then
         self.UpdateValue = UpdateValue_Horizontal
         AF.SetPoint(self.fill.mask, "TOPLEFT")
         AF.SetPoint(self.fill.mask, "BOTTOMLEFT")
-        AF.SetPoint(self.loss.mask, "TOPLEFT", self.fill.mask, "TOPRIGHT")
-        AF.SetPoint(self.loss.mask, "BOTTOMRIGHT")
+        AF.SetPoint(self.unfill.mask, "TOPLEFT", self.fill.mask, "TOPRIGHT")
+        AF.SetPoint(self.unfill.mask, "BOTTOMRIGHT")
     elseif orientation == "right_to_left" then
         self.UpdateValue = UpdateValue_Horizontal
         AF.SetPoint(self.fill.mask, "TOPRIGHT")
         AF.SetPoint(self.fill.mask, "BOTTOMRIGHT")
-        AF.SetPoint(self.loss.mask, "BOTTOMRIGHT", self.fill.mask, "BOTTOMLEFT")
-        AF.SetPoint(self.loss.mask, "TOPLEFT")
+        AF.SetPoint(self.unfill.mask, "BOTTOMRIGHT", self.fill.mask, "BOTTOMLEFT")
+        AF.SetPoint(self.unfill.mask, "TOPLEFT")
     elseif orientation == "bottom_to_top" then
         self.UpdateValue = UpdateValue_Vertical
         AF.SetPoint(self.fill.mask, "BOTTOMLEFT")
         AF.SetPoint(self.fill.mask, "BOTTOMRIGHT")
-        AF.SetPoint(self.loss.mask, "BOTTOMRIGHT", self.fill.mask, "TOPRIGHT")
-        AF.SetPoint(self.loss.mask, "TOPLEFT")
+        AF.SetPoint(self.unfill.mask, "BOTTOMRIGHT", self.fill.mask, "TOPRIGHT")
+        AF.SetPoint(self.unfill.mask, "TOPLEFT")
     elseif orientation == "top_to_bottom" then
         self.UpdateValue = UpdateValue_Vertical
         AF.SetPoint(self.fill.mask, "TOPLEFT")
         AF.SetPoint(self.fill.mask, "TOPRIGHT")
-        AF.SetPoint(self.loss.mask, "TOPLEFT", self.fill.mask, "BOTTOMLEFT")
-        AF.SetPoint(self.loss.mask, "BOTTOMRIGHT")
+        AF.SetPoint(self.unfill.mask, "TOPLEFT", self.fill.mask, "BOTTOMLEFT")
+        AF.SetPoint(self.unfill.mask, "BOTTOMRIGHT")
     end
 
     self:UpdateValue()
@@ -279,18 +323,6 @@ function AF_SimpleStatusBarMixin:GetRemainingValue()
     return self.max - self.value
 end
 
-function AF_SimpleStatusBarMixin:GetBarSize()
-    return self.inner:GetSize()
-end
-
-function AF_SimpleStatusBarMixin:GetBarWidth()
-    return self.inner:GetWidth()
-end
-
-function AF_SimpleStatusBarMixin:GetBarHeight()
-    return self.inner:GetHeight()
-end
-
 -- set
 function AF_SimpleStatusBarMixin:SetMinMaxValues(min, max)
     self.min = min
@@ -308,44 +340,40 @@ function AF_SimpleStatusBarMixin:Dim(enabled)
     self.mod:SetShown(enabled)
 end
 
--- pixel perfect
-function AF_SimpleStatusBarMixin:DefaultUpdatePixels()
-    AF.ReSize(self)
-    AF.RePoint(self)
-    AF.ReBorder(self)
-    AF.RePoint(self.inner)
-    AF.RePoint(self.fill)
-    AF.RePoint(self.fill.mask)
-    AF.RePoint(self.loss)
-    AF.RePoint(self.loss.mask)
-end
-
 ---@return AF_SimpleStatusBar bar
 function AF.CreateSimpleStatusBar(parent, name, noBackdrop)
-    local bar = CreateFrame("Frame", name, parent)
-    Mixin(bar, AF_BaseWidgetMixin)
-    Mixin(bar, AF_SimpleStatusBarMixin)
+    local frame = CreateFrame("Frame", name, parent)
+    Mixin(frame, AF_BaseWidgetMixin)
+    Mixin(frame, AF_BaseStatusBarMixin)
+    Mixin(frame, AF_SimpleStatusBarMixin)
+
+    -- smooth
+    Mixin(frame, AF_SmoothStatusBarMixin)
+    frame:SetSmoothing(false)
 
     if noBackdrop then
-        bar.SetBackgroundColor = nil
-        bar.SetBorderColor = nil
+        frame.SetBackgroundColor = nil
+        frame.SetBorderColor = nil
     else
-        AF.ApplyDefaultBackdropWithColors(bar)
+        AF.ApplyDefaultBackdropWithColors(frame)
     end
 
     -- default value
-    bar.min = 0
-    bar.max = 0
-    bar.value = 0
+    frame.min = 0
+    frame.max = 0
+    frame.value = 0
 
-    -- smooth
-    Mixin(bar, AF_SmoothStatusBarMixin)
-    bar:SetSmoothing(false)
+    -- innerBar
+    local bar = CreateFrame("Frame", nil, frame)
+    frame.innerBar = bar
+    AF.SetFrameLevel(bar, 0)
 
     -- fill texture
-    local fill = bar:CreateTexture(nil, "BORDER", nil, -1)
-    bar.fill = fill
-    fill.mask = bar:CreateMaskTexture()
+    local fill = frame:CreateTexture(nil, "BORDER", nil, -1)
+    frame.fill = fill
+    fill:SetAllPoints(bar)
+
+    fill.mask = frame:CreateMaskTexture()
     fill.mask:SetTexture(AF.GetPlainTexture(), "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE", "NEAREST")
     fill:AddMaskTexture(fill.mask)
 
@@ -353,31 +381,29 @@ function AF.CreateSimpleStatusBar(parent, name, noBackdrop)
     -- fill:SetTexelSnappingBias(0)
     -- fill:SetSnapToPixelGrid(false)
 
-    -- loss texture
-    local loss = bar:CreateTexture(nil, "BORDER", nil, -1)
-    bar.loss = loss
-    loss.mask = bar:CreateMaskTexture()
-    loss.mask:SetTexture(AF.GetPlainTexture(), "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE", "NEAREST")
-    loss:AddMaskTexture(loss.mask)
+    -- unfill texture
+    local unfill = frame:CreateTexture(nil, "BORDER", nil, -1)
+    frame.unfill = unfill
+    unfill:SetAllPoints(bar)
 
-    -- inner texture NOTE: for getting bar size/width/height
-    local inner = bar:CreateTexture(nil, "BORDER", nil, -2)
-    bar.inner = inner
+    unfill.mask = frame:CreateMaskTexture()
+    unfill.mask:SetTexture(AF.GetPlainTexture(), "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE", "NEAREST")
+    unfill:AddMaskTexture(unfill.mask)
 
     -- dim
-    local mod = bar:CreateTexture(nil, "ARTWORK", nil, 1)
-    bar.mod = mod
+    local mod = frame:CreateTexture(nil, "ARTWORK", nil, 1)
+    frame.mod = mod
     mod:SetAllPoints(fill.mask)
     mod:SetColorTexture(0.6, 0.6, 0.6)
     mod:SetBlendMode("MOD")
     mod:Hide()
 
     -- setup border and orientation
-    bar:EnableBorder(not noBackdrop)
-    bar:SetOrientation("left_to_right")
+    frame:EnableBorder(not noBackdrop)
+    frame:SetOrientation("left_to_right")
 
     -- pixel perfect
-    AF.AddToPixelUpdater_Auto(bar, bar.DefaultUpdatePixels)
+    AF.AddToPixelUpdater_Auto(frame, frame.DefaultUpdatePixels)
 
-    return bar
+    return frame
 end
