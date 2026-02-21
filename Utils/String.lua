d818b6ea7b2c19dd6b0fde873fe4253c35626aea
enderneko <enderneko-dev@outlook.com>
2026-02-21 13:38:24 +0800

    Wrap secret number formatting in AF.isRetail check

--- Utils/String.lua
+++ Utils/String.lua
@@ -151,56 +151,61 @@ function AF.FormatNumber(n)
 end
 
 ---------------------------------------------------------------------
--- number format (secret)
+-- secret
 ---------------------------------------------------------------------
-local AbbreviateNumbers = AbbreviateNumbers
-local CreateAbbreviateConfig = CreateAbbreviateConfig
+if AF.isRetail then
+    --------------------------------------------------
+    -- number format (secret)
+    --------------------------------------------------
+    local AbbreviateNumbers = AbbreviateNumbers
+    local CreateAbbreviateConfig = CreateAbbreviateConfig
 
-local asianNumberAbbrevOptions = {
-    -- breakpointData = {
-    --     {breakpoint = 100000000, abbreviation = abbr_100000K, significandDivisor = 1000000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23亿
-    --     {breakpoint = 10000, abbreviation = abbr_10K, significandDivisor = 1000, fractionDivisor = 10, abbreviationIsGlobal = false}, -- 1.2万
-    -- },
-    config = CreateAbbreviateConfig({ -- use a cached config for optimal performance
-        {breakpoint = 100000000, abbreviation = abbr_100000K, significandDivisor = 1000000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23亿
-        {breakpoint = 10000, abbreviation = abbr_10K, significandDivisor = 1000, fractionDivisor = 10, abbreviationIsGlobal = false}, -- 1.2万
-    })
-}
+    local asianNumberAbbrevOptions = {
+        -- breakpointData = {
+        --     {breakpoint = 100000000, abbreviation = abbr_100000K, significandDivisor = 1000000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23亿
+        --     {breakpoint = 10000, abbreviation = abbr_10K, significandDivisor = 1000, fractionDivisor = 10, abbreviationIsGlobal = false}, -- 1.2万
+        -- },
+        config = CreateAbbreviateConfig({ -- use a cached config for optimal performance
+            {breakpoint = 100000000, abbreviation = abbr_100000K, significandDivisor = 1000000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23亿
+            {breakpoint = 10000, abbreviation = abbr_10K, significandDivisor = 1000, fractionDivisor = 10, abbreviationIsGlobal = false}, -- 1.2万
+        })
+    }
 
-local westernNumberAbbrevOptions = {
-    -- breakpointData = {
-    --     {breakpoint = 1000000000, abbreviation = "B", significandDivisor = 10000000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23B
-    --     {breakpoint = 1000000, abbreviation = "M", significandDivisor = 10000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23M
-    --     {breakpoint = 1000, abbreviation = "K", significandDivisor = 100, fractionDivisor = 10, abbreviationIsGlobal = false}, -- 1.2K
-    -- },
-    config = CreateAbbreviateConfig({ -- use a cached config for optimal performance
-        {breakpoint = 1000000000, abbreviation = "B", significandDivisor = 10000000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23B
-        {breakpoint = 1000000, abbreviation = "M", significandDivisor = 10000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23M
-        {breakpoint = 1000, abbreviation = "K", significandDivisor = 100, fractionDivisor = 10, abbreviationIsGlobal = false}, -- 1.2K
-    })
-}
+    local westernNumberAbbrevOptions = {
+        -- breakpointData = {
+        --     {breakpoint = 1000000000, abbreviation = "B", significandDivisor = 10000000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23B
+        --     {breakpoint = 1000000, abbreviation = "M", significandDivisor = 10000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23M
+        --     {breakpoint = 1000, abbreviation = "K", significandDivisor = 100, fractionDivisor = 10, abbreviationIsGlobal = false}, -- 1.2K
+        -- },
+        config = CreateAbbreviateConfig({ -- use a cached config for optimal performance
+            {breakpoint = 1000000000, abbreviation = "B", significandDivisor = 10000000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23B
+            {breakpoint = 1000000, abbreviation = "M", significandDivisor = 10000, fractionDivisor = 100, abbreviationIsGlobal = false}, -- 1.23M
+            {breakpoint = 1000, abbreviation = "K", significandDivisor = 100, fractionDivisor = 10, abbreviationIsGlobal = false}, -- 1.2K
+        })
+    }
 
-function AF.FormatSecretNumber_Asian(n)
-    return AbbreviateNumbers(n, asianNumberAbbrevOptions)
-end
+    function AF.FormatSecretNumber_Asian(n)
+        return AbbreviateNumbers(n, asianNumberAbbrevOptions)
+    end
 
-function AF.FormatSecretNumber(n)
-    return AbbreviateNumbers(n, westernNumberAbbrevOptions)
-end
+    function AF.FormatSecretNumber(n)
+        return AbbreviateNumbers(n, westernNumberAbbrevOptions)
+    end
 
----------------------------------------------------------------------
--- percentage format (secret)
----------------------------------------------------------------------
-local percentageAbbrevOptions = {
-    config = CreateAbbreviateConfig({ -- use a cached config for optimal performance
-        {breakpoint = 0.0000000000001, abbreviation = "", significandDivisor = 0.1, fractionDivisor = 10, abbreviationIsGlobal = false}, -- 1.2%
-    })
-}
+    --------------------------------------------------
+    -- percentage format (secret)
+    --------------------------------------------------
+    local percentageAbbrevOptions = {
+        config = CreateAbbreviateConfig({ -- use a cached config for optimal performance
+            {breakpoint = 0.0000000000001, abbreviation = "", significandDivisor = 0.1, fractionDivisor = 10, abbreviationIsGlobal = false}, -- 1.2%
+        })
+    }
 
--- this function produces percentages with one decimal place.
--- format("%.1f%%", n) would produce "1.0%" for 1.0, but AbbreviateNumbers would produce "1%".
-function AF.FormatSecretPercentage(n)
-    return AbbreviateNumbers(n, percentageAbbrevOptions)
+    -- this function produces percentages with one decimal place.
+    -- format("%.1f%%", n) would produce "1.0%" for 1.0, but AbbreviateNumbers would produce "1%".
+    function AF.FormatSecretPercentage(n)
+        return AbbreviateNumbers(n, percentageAbbrevOptions)
+    end
 end
 
 ---------------------------------------------------------------------
